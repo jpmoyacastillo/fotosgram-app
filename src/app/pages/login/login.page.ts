430436eb80d08b2c99a58736e2e33cd16755eea0
JP <juanpablo@rindegastos.com>
2022-09-16 18:14:56 +0200

    Init app and posts, post and login pages done

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IonSlides } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  @ViewChild('slidePrincipal') slides: IonSlides;
+
+  avatars = [
+    {
+      img: 'av-1.png',
+      seleccionado: true,
+    },
+    {
+      img: 'av-2.png',
+      seleccionado: false,
+    },
+    {
+      img: 'av-3.png',
+      seleccionado: false,
+    },
+    {
+      img: 'av-4.png',
+      seleccionado: false,
+    },
+    {
+      img: 'av-5.png',
+      seleccionado: false,
+    },
+    {
+      img: 'av-6.png',
+      seleccionado: false,
+    },
+    {
+      img: 'av-7.png',
+      seleccionado: false,
+    },
+    {
+      img: 'av-8.png',
+      seleccionado: false,
+    },
+  ];
+
+  avatarSlide = {
+    slidesPerView: 3.5,
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  ionViewDidEnter() {
+    this.slides.lockSwipes(true);
+  }
+
+  login(fLogin: NgForm) {
+    console.log(fLogin.valid);
+  }
+
+  registro(fRegistro: NgForm) {
+    console.log(fRegistro.valid);
+  }
+
+  seleccionarAvatar(avatar) {
+    this.avatars.forEach((av) => (av.seleccionado = false));
+
+    avatar.seleccionado = true;
+  }
+
+  mostrarRegistro() {
+    this.slides.lockSwipes(false);
+    this.slides.slideTo(1);
+    this.slides.lockSwipes(true);
+  }
+
+  mostrarLogin() {
+    this.slides.lockSwipes(false);
+    this.slides.slideTo(0);
+    this.slides.lockSwipes(true);
+  }
+}
